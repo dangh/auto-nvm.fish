b49e8a40f77202117abbd60304c5db55c0bd8d7e
Dang <huynhminhdang@gmail.com>
2024-03-14 08:15:44 +0800

    format code with fish_indent

--- conf.d/nvm_autoload.fish
+++ conf.d/nvm_autoload.fish
@@ -1,70 +1,70 @@
 function _nvm_find_up_multi
-  set -l dir $argv[1]
-  set -l files $argv[2..]
-  for file in $files
-    if test -e "$dir/$file"
-      echo $dir/$file
+    set -l dir $argv[1]
+    set -l files $argv[2..]
+    for file in $files
+        if test -e "$dir/$file"
+            echo $dir/$file
+        end
     end
-  end
-  if test "$dir" != '/'
-    _nvm_find_up_multi (path dirname $dir) $files
-  end
+    if test "$dir" != /
+        _nvm_find_up_multi (path dirname $dir) $files
+    end
 end
 
 function nvm-activate
-  set -l v_fallback lts
+    set -l v_fallback lts
 
-  #nvm_current_version might be exported to this process
-  set -q nvm_current_version && ! type -q node && set -e nvm_current_version
+    #nvm_current_version might be exported to this process
+    set -q nvm_current_version && ! type -q node && set -e nvm_current_version
 
-  #load nvm and it's utility functions
-  type nvm >/dev/null || nvm --help >/dev/null
+    #load nvm and it's utility functions
+    type nvm >/dev/null || nvm --help >/dev/null
 
-  #read version from nvm files
-  _nvm_find_up_multi $PWD .nvmrc .node-version serverless.yml | while read -l file
-    switch $file
-    case "*/.nvmrc" "*/.node-version"
-      read v <$file
-      break
-    case "*/serverless.yml"
-      if string match -q -r '^\s*runtime:\s*nodejs(?<v>\d+).x' <$file
-        break
-      end
+    #read version from nvm files
+    _nvm_find_up_multi $PWD .nvmrc .node-version serverless.yml | while read -l file
+        switch $file
+            case "*/.nvmrc" "*/.node-version"
+                read v <$file
+                break
+            case "*/serverless.yml"
+                if string match -q -r '^\s*runtime:\s*nodejs(?<v>\d+).x' <$file
+                    break
+                end
+        end
     end
-  end
 
-  #if there's no nvm files, use default version
-  test -n "$v" || set v $nvm_default_version
+    #if there's no nvm files, use default version
+    test -n "$v" || set v $nvm_default_version
 
-  #if there's no default version, use fallback version
-  test -n "$v" || set v $v_fallback
+    #if there's no default version, use fallback version
+    test -n "$v" || set v $v_fallback
 
-  #resolve v to specific version
-  string match -e -r (_nvm_version_match $v) <$nvm_data/.index | read v __
+    #resolve v to specific version
+    string match -e -r (_nvm_version_match $v) <$nvm_data/.index | read v __
 
-  #do nothing if version is not changed
-  test "$v" = "$nvm_current_version" && return
+    #do nothing if version is not changed
+    test "$v" = "$nvm_current_version" && return
 
-  #install if necessary
-  _nvm_list | string match -e -r (_nvm_version_match $v) | read v_installed __
-  if test -n "$v_installed"
-    nvm use $v_installed >/dev/null
-  else
-    nvm install $v >/dev/null
-  end
+    #install if necessary
+    _nvm_list | string match -e -r (_nvm_version_match $v) | read v_installed __
+    if test -n "$v_installed"
+        nvm use $v_installed >/dev/null
+    else
+        nvm install $v >/dev/null
+    end
 
-  #print nvm message
-  status is-interactive && ! set -q auto_nvm_quiet && printf "Now using Node %s (npm %s) %s\n" (_nvm_node_info)
+    #print nvm message
+    status is-interactive && ! set -q auto_nvm_quiet && printf "Now using Node %s (npm %s) %s\n" (_nvm_node_info)
 end
 
 if status is-interactive
-  function nvm-autoload -e fish_prompt
-    function nvm-autoload -v PWD
-      nvm-activate
-    end && nvm-autoload
-  end
+    function nvm-autoload -e fish_prompt
+        function nvm-autoload -v PWD
+            nvm-activate
+        end && nvm-autoload
+    end
 else
-  function nvm-autoload -v PWD
-    nvm-activate
-  end && nvm-autoload
+    function nvm-autoload -v PWD
+        nvm-activate
+    end && nvm-autoload
 end
